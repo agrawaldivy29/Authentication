348b30fdd58ecca75c95e0fad91613a6ea29e6ed
Divy Agrawal <divyagrawal29@gmail.com>
2023-11-20 22:27:21 +0530

    Changed type to module in package.json

--- app.js
+++ app.js
@@ -1,74 +1,75 @@
-require("dotenv").config(); 
-const express = require('express');
-const ejs = require('ejs');
-const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const app = express();
+import "dotenv/config";
+import express from "express";
+import ejs from "ejs";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+const app = express();
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.set('strictQuery', false);
-main().catch(err => console.log(err));
+mongoose.set("strictQuery", false);
+main().catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String,
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home.ejs");
 });
 
 app.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login.ejs");
 });
 
 app.post("/login", (req, res) => {
-    const userName = req.body.username;
-    const password = req.body.password;
+  const userName = req.body.username;
+  const password = req.body.password;
 
-    User.findOne({email: userName}, (err, foundUser) => {
-        if (err) console.log(err);
-        else 
-        {
-            if (foundUser)
-            {
-                if(foundUser.password === password) 
-                {
-                    res.render("Secrets");
-                }
-            }
+  User.findOne({ email: userName }, (err, foundUser) => {
+    if (err) console.log(err);
+    else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("Secrets.ejs");
         }
-    })
-})
+      }
+    }
+  });
+});
 
 app.get("/register", (req, res) => {
-    res.render("register");
+  res.render("registerejs");
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
 
-    newUser.save((err) => {
-        if (err) console.log(err);
-        else res.render("secrets");
-    })
-})
+  newUser.save((err) => {
+    if (err) console.log(err);
+    else res.render("secrets.ejs");
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
